25a24237871f98f75c44e601d185d61a69c8736e
duyongquan <quandy2020@126.com>
2022-11-24 18:22:18 +0800

    [Add] 添加service的example
    	1 添加service的example
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- service/include/service/tutorials_service_demo1_initial_env.hpp
+++ service/include/service/tutorials_service_demo1_initial_env.hpp
@@ -7,25 +7,48 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "nav_msgs/msg/path.hpp"
+#include "tutorials_msgs/srv/add_two_ints.hpp"
 
 namespace ros2_tutorials
 {
-namespace rviz
+namespace service
 {
 
+using AddTwoInts = tutorials_msgs::srv::AddTwoInts;
 
-class ShowCircleTrajectory : public rclcpp::Node
+class Client : public rclcpp::Node
 {
 public:
-    ShowCircleTrajectory();
-    ~ShowCircleTrajectory();
+    Client();
+    ~Client();
+
+    bool ConnectServer(const std::chrono::seconds & timeout);
+    void SendMessage();
 
 private:
+    void HandleTimerCallback();
+
+    rclcpp::Client<AddTwoInts>::SharedPtr client_{nullptr};
+    rclcpp::TimerBase::SharedPtr timer_ {nullptr};
+    bool connect_{false};
 
 };
 
-}  // namespace rviz
+class Server : public rclcpp::Node
+{
+public:
+    Server();
+    ~Server();
+
+private:
+    void HandleClientRequest(
+        const std::shared_ptr<AddTwoInts::Request> request,
+        const std::shared_ptr<AddTwoInts::Response> response);
+
+    rclcpp::Service<AddTwoInts>::SharedPtr server_{nullptr};
+};
+
+}  // namespace service
 }  // namespace ros2_tutorials
 
 #endif  // ROS2_TUTORIALS__TUTORIALS_SERVICE_DEMO1_INITIAL_ENV_HPP_
