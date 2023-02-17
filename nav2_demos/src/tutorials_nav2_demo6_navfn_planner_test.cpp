37ac60a3f0676a36c90426b6a88cd3fe69320018
duyongquan <quandy2020@126.com>
2023-02-17 08:51:34 +0800

    [Fix] add nav2_fn planner test
    	1 add nav2_fn planner test
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ nav2_demos/src/tutorials_nav2_demo6_navfn_planner_test.cpp
@@ -0,0 +1,65 @@
+#include <chrono>
+#include <memory>
+#include <string>
+#include <cmath>
+#include <Eigen/Dense>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/path.hpp"
+
+#include "nav2_demos/tutorials_nav2_utils_opencv_to_costmap.hpp"
+#include "nav2_demos/tutorials_nav2_utils_dataset_loader.hpp"
+
+using namespace std::chrono_literals;
+
+namespace ros2_tutorials
+{
+namespace nav2
+{
+namespace
+{
+
+class NavFnPlanner : public rclcpp::Node
+{
+public:
+    NavFnPlanner() : Node("navfn_planner")
+    {
+        costmap_2d_ = std::make_shared<ImageConvertCostmap2D>(this, "global_map");
+
+        timer_ = create_wall_timer(
+            1000ms, std::bind(&NavFnPlanner::HandleTimerCallback, this));
+    }
+
+    ~NavFnPlanner() {}
+
+private:
+    void HandleTimerCallback()
+    {
+        // ShowImage();
+        // auto map = costmap_2d_->GetMap("map03.pgm");
+        // map.header.frame_id = "map";
+        // map.header.stamp = this->get_clock()->now();
+        // costmap_2d_->PublishMap(map);
+    }
+
+
+    rclcpp::TimerBase::SharedPtr timer_{nullptr};
+    std::shared_ptr<ImageConvertCostmap2D> costmap_2d_{nullptr};
+};
+
+
+}  // namespace
+}  // namespace nav2
+}  // namespace ros2_tutorials
+
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ros2_tutorials::nav2::NavFnPlanner>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
