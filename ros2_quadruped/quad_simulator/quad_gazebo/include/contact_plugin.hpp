ebe0799566ad4563aba02fd1478e707a8a53c438
duyongquan <quandy2020@126.com>
2023-03-10 17:22:12 +0800

    [Fix] Fix ros package quad_gazebo
    	1 Fix ros package quad_gazebo
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ ros2_quadruped/quad_simulator/quad_gazebo/include/contact_plugin.hpp
@@ -0,0 +1,20 @@
+
+#ifndef ROS2_QUADRUPED__QUAD_SIMULATOR_QUAD_GAZEBO_CONTACT_PLUGIN_HPP_
+#define ROS2_QUADRUPED__QUAD_SIMULATOR_QUAD_GAZEBO_CONTACT_PLUGIN_HPP_
+
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+namespace ros2_quadruped
+{
+namespace quad_gazebo
+{
+
+}  // namespace quad_gazebo
+}  // namespace ros2_quadruped
+
+#endif  // ROS2_QUADRUPED__QUAD_SIMULATOR_QUAD_GAZEBO_CONTACT_PLUGIN_HPP_
