f492b75e285e5770d285fa31c1106f41299791b1
duyongquan <quandy2020@126.com>
2023-03-10 19:08:26 +0800

    [Fix] msg
    	1 msg
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- ros2_quadruped/quad_simulator/quad_gazebo/include/contact_plugin.hpp
+++ ros2_quadruped/quad_simulator/quad_gazebo/include/contact_plugin.hpp
@@ -9,12 +9,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-namespace ros2_quadruped
-{
-namespace quad_gazebo
-{
 
-}  // namespace quad_gazebo
-}  // namespace ros2_quadruped
 
 #endif  // ROS2_QUADRUPED__QUAD_SIMULATOR_QUAD_GAZEBO_CONTACT_PLUGIN_HPP_
