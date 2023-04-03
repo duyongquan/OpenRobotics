d6b06bd0c09dd5cca23814f578dba4119c20ea61
duyongquan <quandy2020@126.com>
2023-04-03 17:01:46 +0800

    [Fix] add markder
    	1 add markder: cube test
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ ros2_basics/rviz/src/tutorials_rviz_marker_cube_test.cpp
@@ -0,0 +1,201 @@
+// Copyright (c) 2011, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Willow Garage, Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "geometry_msgs/msg/pose.hpp"
+#include "interactive_markers/interactive_marker_server.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2/LinearMath/Vector3.h"
+#include "visualization_msgs/msg/interactive_marker.hpp"
+#include "visualization_msgs/msg/interactive_marker_control.hpp"
+#include "visualization_msgs/msg/interactive_marker_feedback.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+namespace ros2_tutorials
+{
+namespace rviz
+{
+
+class CubeNode : public rclcpp::Node
+{
+public:
+    explicit CubeNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+    ~CubeNode() = default;
+
+private:
+    void processFeedback(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr & feedback);
+
+    visualization_msgs::msg::InteractiveMarkerControl & makeBoxControl(
+    visualization_msgs::msg::InteractiveMarker & msg);
+
+    void makeCube();
+
+    std::unique_ptr<interactive_markers::InteractiveMarkerServer> server_;
+    std::vector<tf2::Vector3> positions_;
+};  // class CubeNode
+
+CubeNode::CubeNode(const rclcpp::NodeOptions & options)
+: rclcpp::Node("cube", options)
+{
+    server_ = std::make_unique<interactive_markers::InteractiveMarkerServer>(
+        "cube",
+        get_node_base_interface(),
+        get_node_clock_interface(),
+        get_node_logging_interface(),
+        get_node_topics_interface(),
+        get_node_services_interface());
+    makeCube();
+    server_->applyChanges();
+    RCLCPP_INFO(get_logger(), "Ready");
+}
+
+void CubeNode::processFeedback(
+    const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr & feedback)
+{
+    switch (feedback->event_type) {
+        case visualization_msgs::msg::InteractiveMarkerFeedback::POSE_UPDATE:
+            {
+                // compute difference vector for this cube
+                tf2::Vector3 fb_pos(
+                    feedback->pose.position.x, feedback->pose.position.y, feedback->pose.position.z);
+                std::size_t index = static_cast<std::size_t>(std::stoi(feedback->marker_name));
+
+                if (index > positions_.size()) {
+                    return;
+                }
+
+                tf2::Vector3 fb_delta = fb_pos - positions_[index];
+
+                // move all markers in that direction
+                for (std::size_t i = 0; i < positions_.size(); ++i) {
+                    double d = fb_pos.distance(positions_[i]);
+                    double t = 1 / ( d * 5.0 + 1.0) - 0.2;
+                    if (t < 0.0) {
+                    t = 0.0;
+                    }
+
+                    positions_[i] += t * fb_delta;
+
+                    if (i == index) {
+                    positions_[i] = fb_pos;
+                    }
+
+                    geometry_msgs::msg::Pose pose;
+                    pose.position.x = positions_[i].x();
+                    pose.position.y = positions_[i].y();
+                    pose.position.z = positions_[i].z();
+
+                    server_->setPose(std::to_string(i), pose);
+                }
+                break;
+            }
+    }
+    server_->applyChanges();
+}
+
+visualization_msgs::msg::InteractiveMarkerControl& CubeNode::makeBoxControl(
+    visualization_msgs::msg::InteractiveMarker & msg)
+{
+    visualization_msgs::msg::InteractiveMarkerControl control;
+    control.always_visible = true;
+    control.orientation_mode = visualization_msgs::msg::InteractiveMarkerControl::VIEW_FACING;
+    control.interaction_mode = visualization_msgs::msg::InteractiveMarkerControl::MOVE_PLANE;
+    control.independent_marker_orientation = true;
+
+    visualization_msgs::msg::Marker marker;
+
+    marker.type = visualization_msgs::msg::Marker::CUBE;
+    marker.scale.x = msg.scale;
+    marker.scale.y = msg.scale;
+    marker.scale.z = msg.scale;
+    marker.color.r = static_cast<float>(0.65 + 0.7 * msg.pose.position.x);
+    marker.color.g = static_cast<float>(0.65 + 0.7 * msg.pose.position.y);
+    marker.color.b = static_cast<float>(0.65 + 0.7 * msg.pose.position.z);
+    marker.color.a = 1.0f;
+
+    control.markers.push_back(marker);
+    msg.controls.push_back(control);
+
+    return msg.controls.back();
+}
+
+void CubeNode::makeCube()
+{
+    const int side_length = 10;
+    const double step = 1.0 / static_cast<double>(side_length);
+    int count = 0;
+
+    positions_.reserve(side_length * side_length * side_length);
+
+    for (double x = -0.5; x < 0.5; x += step) {
+        for (double y = -0.5; y < 0.5; y += step) {
+            for (double z = 0.0; z < 1.0; z += step) {
+                visualization_msgs::msg::InteractiveMarker int_marker;
+                int_marker.header.frame_id = "base_link";
+                int_marker.scale = static_cast<float>(step);
+
+                int_marker.pose.position.x = x;
+                int_marker.pose.position.y = y;
+                int_marker.pose.position.z = z;
+
+                positions_.push_back(tf2::Vector3(x, y, z));
+
+                int_marker.name = std::to_string(count);
+
+                makeBoxControl(int_marker);
+
+                server_->insert(int_marker);
+                server_->setCallback(
+                int_marker.name,
+                std::bind(&CubeNode::processFeedback, this, std::placeholders::_1));
+
+                count++;
+            }
+        }
+    }
+}
+
+}  // namespace rviz
+}  // namespace ros2_tutorials
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ros2_tutorials::rviz::CubeNode>();
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin();
+  rclcpp::shutdown();
+  return 0;
+}
+
---
+++ ros2_basics/rviz/src/tutorials_rviz_marker_simple_marker_test.cpp
@@ -0,0 +1,100 @@
+#include <memory>
+#include <sstream>
+
+#include "interactive_markers/interactive_marker_server.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "visualization_msgs/msg/interactive_marker.hpp"
+#include "visualization_msgs/msg/interactive_marker_control.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+using namespace std::chrono_literals;
+
+namespace ros2_tutorials
+{
+namespace rviz
+{
+
+class SimpleMarker : public rclcpp::Node
+{
+public:
+    SimpleMarker() : Node("simple_marker")
+    {
+        server_ = std::make_shared<interactive_markers::InteractiveMarkerServer>(
+            "simple_marker", this);
+        
+        // create an interactive marker for our server
+        visualization_msgs::msg::InteractiveMarker interactive_marker;
+        interactive_marker.header.frame_id = "base_link";
+        interactive_marker.header.stamp = this->get_clock()->now();
+        interactive_marker.name = "my_marker";
+        interactive_marker.description = "Simple 1-DOF Control";
+
+        // create a grey box marker
+        visualization_msgs::msg::Marker box_marker;
+        box_marker.type = visualization_msgs::msg::Marker::CUBE;
+        box_marker.scale.x = 0.45;
+        box_marker.scale.y = 0.45;
+        box_marker.scale.z = 0.45;
+        box_marker.color.r = 0.5;
+        box_marker.color.g = 0.5;
+        box_marker.color.b = 0.5;
+        box_marker.color.a = 1.0;
+
+        // create a non-interactive control which contains the box
+        visualization_msgs::msg::InteractiveMarkerControl box_control;
+        box_control.always_visible = true;
+        box_control.markers.push_back(box_marker);
+
+        // add the control to the interactive marker
+        interactive_marker.controls.push_back(box_control);
+
+        // create a control which will move the box
+        // this control does not contain any markers,
+        // which will cause RViz to insert two arrows
+        visualization_msgs::msg::InteractiveMarkerControl rotate_control;
+        rotate_control.name = "move_x";
+        rotate_control.interaction_mode = visualization_msgs::msg::InteractiveMarkerControl::MOVE_AXIS;
+
+        // add the control to the interactive marker
+        interactive_marker.controls.push_back(rotate_control);
+
+        // add the interactive marker to our collection &
+        // tell the server to call HandleProcessCallback() when feedback arrives for it
+        server_->insert(interactive_marker,
+            std::bind(&SimpleMarker::HandleProcessCallback, this, std::placeholders::_1));
+
+        // 'commit' changes and send to all clients
+        server_->applyChanges();
+
+        RCLCPP_INFO(this->get_logger(), "Ready");
+    }
+
+    ~SimpleMarker() {}
+
+private:
+    void HandleProcessCallback(const visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr & feedback)
+    {
+        std::ostringstream oss;
+        oss << feedback->marker_name << " is now at " 
+            << feedback->pose.position.x << ", " 
+            << feedback->pose.position.y << ", " 
+            << feedback->pose.position.z;
+
+        RCLCPP_INFO(this->get_logger(), "%s", oss.str().c_str());
+    }
+
+    // create an interactive marker server on the namespace "simple_marker"
+    std::shared_ptr<interactive_markers::InteractiveMarkerServer> server_{nullptr};
+};
+
+}  // namespace rviz
+}  // namespace ros2_tutorials
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ros2_tutorials::rviz::SimpleMarker>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
