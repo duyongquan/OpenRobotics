f486d823d780479915437ec20a215c709b1ff52f
duyongquan <quandy2020@126.com>
2023-07-07 16:57:44 +0800

    [Add] Fix readme doc
    	1 Fix readme doc
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- ros2_basics/rviz/src/tutorials_rviz_marker_points_and_lines_test.cpp
+++ ros2_basics/rviz/src/tutorials_rviz_marker_points_and_lines_test.cpp
@@ -29,7 +29,7 @@ private:
     void HandleTimerCallback()
     {
         visualization_msgs::msg::Marker points, line_strip, line_list;
-        points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/my_frame";
+        points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/map";
         points.header.stamp = line_strip.header.stamp = line_list.header.stamp = rclcpp::Clock().now();
         points.ns = line_strip.ns = line_list.ns = "points_and_lines";
         points.action = line_strip.action = line_list.action = visualization_msgs::msg::Marker::ADD;
