59457f6d075cd8424bc2ff98554cd11373f88d69
duyongquan <quandy2020@126.com>
2023-11-02 16:35:14 +0800

    [Fix] Add quadrotor_msgs
    	1 Add quadrotor_msgs
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ ros2_navigation/uav_simulator/quadrotor_msgs/include/quadrotor_msgs/decode_msgs.h
@@ -0,0 +1,23 @@
+#ifndef __QUADROTOR_MSGS_QUADROTOR_MSGS_H__
+#define __QUADROTOR_MSGS_QUADROTOR_MSGS_H__
+
+#include <stdint.h>
+#include <vector>
+#include <quadrotor_msgs/msg/output_data.hpp>
+#include <quadrotor_msgs/msg/status_data.hpp>
+#include <quadrotor_msgs/msg/ppr_output_data.hpp>
+
+namespace quadrotor_msgs
+{
+
+bool decodeOutputData(const std::vector<uint8_t> &data,
+                      quadrotor_msgs::msg::OutputData &output);
+
+bool decodeStatusData(const std::vector<uint8_t> &data,
+                      quadrotor_msgs::msg::StatusData &status);
+
+bool decodePPROutputData(const std::vector<uint8_t> &data,
+                         quadrotor_msgs::msg::PPROutputData &output);
+}
+
+#endif
---
+++ ros2_navigation/uav_simulator/quadrotor_msgs/include/quadrotor_msgs/encode_msgs.h
@@ -0,0 +1,22 @@
+#ifndef __QUADROTOR_MSGS_QUADROTOR_MSGS_H__
+#define __QUADROTOR_MSGS_QUADROTOR_MSGS_H__
+
+#include <stdint.h>
+#include <vector>
+#include <quadrotor_msgs/msg/so3_command.hpp>
+#include <quadrotor_msgs/msg/trpy_command.hpp>
+#include <quadrotor_msgs/msg/gains.hpp>
+
+namespace quadrotor_msgs
+{
+
+void encodeSO3Command(const quadrotor_msgs::msg::SO3Command &so3_command,
+                      std::vector<uint8_t> &output);
+void encodeTRPYCommand(const quadrotor_msgs::msg::TRPYCommand &trpy_command,
+                       std::vector<uint8_t> &output);
+
+void encodePPRGains(const quadrotor_msgs::msg::Gains &gains,
+                    std::vector<uint8_t> &output);
+}
+
+#endif
