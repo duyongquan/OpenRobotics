f6c3e738eb0d12fb98da7aec9daf3af8a3e16509
duyongquan <quandy2020@126.com>
2023-11-10 13:26:42 +0800

    [Add] New tutorials01_test_casadi_env for test
    	1 New tutorials01_test_casadi_env for test
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ ros2_controller/casadi_tutorials/casadi_cpp/src/tutorials01_test_casadi_env.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+
+namespace casadi_cpp {
+namespace {
+
+void Run()
+{
+    std::cout << "Hello World!" << std::endl;
+}
+
+}  // namespace
+}  // namespace casadi_cpp
+
+
+
+int main(int argc, char **argv)
+{
+    casadi_cpp::Run();
+    return 0;
+}
